47e9b5938b798dd1d9282d74cc28585454fda88f
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-29 12:47:24 -0600

    complete exercise

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,10 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num >= 10) {
+    return null;
+  } else {
+    console.log(num);
+    recursiveCount(num + 1);
+  }
 }
 
 if (require.main === module) {
@@ -11,3 +16,15 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// 
+/**function recursiveCount(num = 0) {
+ *  if num is greater than or equal to 10 then return
+ *  else {
+ *    print num
+ *    recursiveCount(num + 1)
+ * }
+ * 
+ * 
+ * In this example, if the number provided is greater than or equal to 10, then we return, doing nothing.
+ * Otherwise, we print the number and call recursiveCount again, but this time incrementing our number by 1.
+ */
